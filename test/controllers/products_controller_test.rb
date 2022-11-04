5a2865e2088c61e07597bc19014bedbc3192bfda
Diego Iturra <diturra@dparadig.com>
2022-11-04 00:26:01 -0300

    Se agregan test y se agregan datos en las fixtures

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -5,10 +5,15 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test 'render a list of products' do
     get products_path()
     assert_response :success
+    assert_select '.product', 2
   end
 
   test 'render a detailed product page' do
-
+    get product_path(products(:ps4)) #ps4 esta definido en la fixture
+    assert_response :success
+    assert_select '.title', 'PS4 fat'
+    assert_select '.description', 'PS4 en buen estado'
+    assert_select '.price', '$300000'
   end
 
 end
